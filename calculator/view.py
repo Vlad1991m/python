95d8867af1cc5154b2fd20f3ed52abb4d4d5886f
Vlad <vlad.melnik@bk.ru>
2022-11-07 23:18:00 +0200

    first commit

---
+++ calculator/view.py
@@ -0,0 +1,12 @@
+def input_string():
+    string = input("Input math operation: ")
+    return string
+
+
+def div_null():
+    print("This operation is invalid!")
+    exit()
+
+
+def print_result(result):
+    print(f'Result = {result}')
